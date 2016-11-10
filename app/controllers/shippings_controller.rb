36396573ae8a88ee35f4992cb7b5dcbf1d89c02e
Carmen Chan <carmenkellychan@gmail.com>
2016-11-10 15:17:39 -0800

    refactor shipping controller

--- app/controllers/shippings_controller.rb
+++ app/controllers/shippings_controller.rb
@@ -1,6 +1,8 @@
 class ShippingsController < ApplicationController
 
   def find_rate
+    results =[]
+
     unless params.keys.include?("weight") && params.keys.include?("origin_zip") && params.keys.include?("dest_zip")
       results = ["please provide params"]
       status = :bad_request
@@ -12,34 +14,56 @@ class ShippingsController < ApplicationController
 
       destination = ActiveShipping::Location.new(country: 'US', zip: params[:dest_zip])
 
-      # ups = ActiveShipping::UPS.new(login: ENV["UPS_LOGIN"], password: ENV["UPS_PWD"], key: ENV["UPS_KEY"])
-      # response = ups.find_rates(origin, destination, packages)
-      #
-      fedex = ActiveShipping::FedEx.new(login: ENV["FEDEX_METER_NUM"], password: ENV["FEDEX_PWD"], key: ENV["FEDEX_KEY"], account: ENV["FEDEX_ACNT"], test: true)
-      response = fedex.find_rates(origin, destination, packages)
+      ups_rates = ups(origin, destination, packages)
 
-      # usps = ActiveShipping::USPS.new(login: ENV["USPS_LOGIN"])
-      #
-      # response = usps.find_rates(origin, destination, packages)
+      fedex_rates = fedex(origin, destination, packages)
 
-      results = parseResponse(response)
+      usps_rates = usps(origin, destination, packages)
+
+      results << parseResponse(ups_rates) << parseResponse(fedex_rates) << parseResponse(usps_rates)
       status = :ok
     end
 
       render :json => results, :status => status
-    # @ups_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
-
-    # @fedex_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
 
   end
 
-  def parseResponse(response)
+  def parseResponse(rates)
     results = []
-    rates = response.rates
     rates.each do |shipping_stuff|
       results << {name: shipping_stuff.service_name, cost: "$#{((shipping_stuff.total_price)/100.0).round(2)}"}
     end
     return results
+  end
+
+  def ups(origin, destination, packages)
+    ups = ActiveShipping::UPS.new(login: ENV["UPS_LOGIN"], password: ENV["UPS_PWD"], key: ENV["UPS_KEY"])
+    ups_response = ups.find_rates(origin, destination, packages)
+
+    return ups_response.rates
+
+  rescue ActiveShipping::ResponseError
+    return []
+  end
+
+  def fedex(origin, destination, packages)
+    fedex = ActiveShipping::FedEx.new(login: ENV["FEDEX_METER_NUM"], password: ENV["FEDEX_PWD"], key: ENV["FEDEX_KEY"], account: ENV["FEDEX_ACNT"], test: true)
+    fedex_response = fedex.find_rates(origin, destination, packages)
+
+    return fedex_response.rates
+
+  rescue ActiveShipping::ResponseError
+    return []
+  end
+
+  def usps(origin, destination, packages)
+    usps = ActiveShipping::USPS.new(login: ENV["USPS_LOGIN"])
+    usps_response = usps.find_rates(origin, destination, packages)
+
+    return usps_response.rates
+
+  rescue ActiveShipping::ResponseError
+    return []
   end
 
 end
